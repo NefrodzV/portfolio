4434c0a5fa32d0e787e774cbf2d0869b4a42d464
Neftaly <neftalyrv23@gmail.com>
2024-08-02 18:44:33 -0400

    made the logic to open and close navbar and also added the overlay only when its mobile

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-console.log("Hello js")
-const menuIcon = document.querySelector(".menu-icon")
-const menu = document.querySelector(".menu")
-const items = document.querySelectorAll(".item")
-const handleMenuClick = () => {
-    menuIcon.toggleAttribute("active")
-    menu.toggleAttribute("show")
-console.log("menu click")
-}
+    const hamburgerButton = document.querySelector('button.hamburger')
+    const navbarCloseButton = document.querySelector('button.close')
+    const navbar = document.querySelector('.navbar')
+    const navItems = document.querySelectorAll('nav.navbar a')
+    const overlay = document.querySelector('.overlay')
+    const handleMenuClick = () => {
+        navbar.toggleAttribute("visible")
+        overlay.toggleAttribute('visible')
+    }
 
+navItems.forEach(item => item.addEventListener("click", handleMenuClick))
+hamburgerButton.addEventListener("click", handleMenuClick)
+navbarCloseButton.addEventListener("click", handleMenuClick)
 
-items.forEach( item => item.addEventListener("click", handleMenuClick))
-menuIcon.addEventListener("click", handleMenuClick)
