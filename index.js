53897d9e0b7b7641bf91e376d204da27616bc5cb
Neftaly <neftalyrv23@gmail.com>
2024-08-03 14:31:23 -0400

    made the logic to open and close the navbar with animations and implemented closing states for the closing attribute

--- index.js
+++ index.js
@@ -3,12 +3,26 @@
     const navbar = document.querySelector('.navbar')
     const navItems = document.querySelectorAll('nav.navbar a')
     const overlay = document.querySelector('.overlay')
-    const handleMenuClick = () => {
-        navbar.toggleAttribute("visible")
+
+    const openNavbar = () => {
+        navbar.toggleAttribute("visible");
         overlay.toggleAttribute('visible')
     }
+    const menuClickHandler = () => {
+        navbar.setAttribute('closing',"")
+        overlay.setAttribute('closing',"")
+        navbar.addEventListener('animationend', () => {
+            console.log('running animation en')
+            navbar.removeAttribute('visible')
+            overlay.removeAttribute('visible')
+            navbar.removeAttribute('closing')
+            overlay.removeAttribute('closing')
+        }, { once: true })
+        
+    }
+    
 
-navItems.forEach(item => item.addEventListener("click", handleMenuClick))
-hamburgerButton.addEventListener("click", handleMenuClick)
-navbarCloseButton.addEventListener("click", handleMenuClick)
+navItems.forEach(item => item.addEventListener("click", menuClickHandler))
+hamburgerButton.addEventListener("click", openNavbar)
+navbarCloseButton.addEventListener("click", menuClickHandler)
 
