7feb4392ce5eb856526b3a2af7a3b0b988b5001d
Neftaly <neftalyrv23@gmail.com>
2024-08-06 18:47:33 -0400

    Made the dropdown styled it and made it functional and responsive

--- index.js
+++ index.js
@@ -3,7 +3,9 @@
     const navbar = document.querySelector('.navbar')
     const navItems = document.querySelectorAll('nav.navbar a')
     const overlay = document.querySelector('.overlay')
-
+    const dropdowns = document.querySelectorAll('.dropdown')
+    const dropdownButtons = document.querySelectorAll('.dropdown button')
+    
     const openNavbar = () => {
         navbar.toggleAttribute("visible");
         overlay.toggleAttribute('visible')
@@ -18,11 +20,31 @@
             navbar.removeAttribute('closing')
             overlay.removeAttribute('closing')
         }, { once: true })
-        
     }
     
 
 navItems.forEach(item => item.addEventListener("click", menuClickHandler))
 hamburgerButton.addEventListener("click", openNavbar)
 navbarCloseButton.addEventListener("click", menuClickHandler)
+dropdownButtons.forEach((element) => {
+    element.addEventListener('click', (e) => {
+        const dropdownContent = e.target.nextElementSibling
+        const closeDropdown = () => {
+            dropdownContent.toggleAttribute('closing')
+            dropdownContent.addEventListener('animationend', () => {
+                dropdownContent.removeAttribute('visible')
+                dropdownContent.removeAttribute('closing')
+                /**  Removing event listeners here because it 
+                 * can be the case that mobile cannot trigger some 
+                 * listeners and are not removed */
+                dropdownContent.removeEventListener('mouseleave', closeDropdown)
+                dropdownContent.removeEventListener('focusout', closeDropdown)
+            }, { once: true })
+        }
+        dropdownContent.toggleAttribute('visible')
+        dropdownContent.addEventListener('mouseleave', closeDropdown ,{once: true})
+        dropdownContent.addEventListener('focusout', closeDropdown, {once: true})
+        dropdownContent.focus()
+    })
+})
 
