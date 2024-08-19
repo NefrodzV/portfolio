02ef168732eededbdaac239e7849d12b8dc5fd7a
Neftaly <neftalyrv23@gmail.com>
2024-08-19 15:43:28 -0400

    Made the form for the contact me section and with logic to handle error if they present themselves

--- index.js
+++ index.js
@@ -1,30 +1,84 @@
-    const hamburgerButton = document.querySelector('button.hamburger')
-    const navbarCloseButton = document.querySelector('button.close')
-    const navbar = document.querySelector('.navbar')
-    const navItems = document.querySelectorAll('nav.navbar a')
-    const overlay = document.querySelector('.overlay')
-    const dropdowns = document.querySelectorAll('.dropdown')
-    const dropdownButtons = document.querySelectorAll('.dropdown button')
-    const skillCategoryDropdownButtons = document.querySelectorAll('.skill-category .dropdown')
+const hamburgerButton = document.querySelector('button.hamburger')
+const navbarCloseButton = document.querySelector('button.close')
+const navbar = document.querySelector('.navbar')
+const navItems = document.querySelectorAll('nav.navbar a')
+const overlay = document.querySelector('.overlay')
+const dropdowns = document.querySelectorAll('.dropdown')
+const dropdownButtons = document.querySelectorAll('.dropdown button')
+const skillCategoryDropdownButtons = document.querySelectorAll('.skill-category .dropdown')
+const contactForm = document.getElementById('contact-form')
+const errorElements = document.querySelectorAll('.error')
+const emailInput = document.getElementById('email')
+const messageTextarea = document.getElementById('message')
+const openNavbar = () => {
+    hamburgerButton.setAttribute('aria-expanded', true)
+    navbar.toggleAttribute("visible");
+    overlay.toggleAttribute('visible')
+}
+const menuClickHandler = () => {
+    hamburgerButton.setAttribute('aria-expanded',false)
+    navbar.setAttribute('closing',"")
+    overlay.setAttribute('closing',"")
+    navbar.addEventListener('animationend', () => {
+        console.log('running animation en')
+        navbar.removeAttribute('visible')
+        overlay.removeAttribute('visible')
+        navbar.removeAttribute('closing')
+        overlay.removeAttribute('closing')
+    }, { once: true })
+}
     
-    const openNavbar = () => {
-        hamburgerButton.setAttribute('aria-expanded', true)
-        navbar.toggleAttribute("visible");
-        overlay.toggleAttribute('visible')
+const onSubmitContactForm = (e) => {
+    e.preventDefault()
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+    const emailError = errorElements[0]
+    const messageError = errorElements[1]
+    const data = Object.fromEntries(new FormData(e.target))
+    let hasErrors = false
+
+    if(data.email.length === 0) {
+        hasErrors = true
+        emailError.textContent = 'Please write your email before submitting!'
+        emailError.setAttribute('visible', "")
+        
     }
-    const menuClickHandler = () => {
-        hamburgerButton.setAttribute('aria-expanded',false)
-        navbar.setAttribute('closing',"")
-        overlay.setAttribute('closing',"")
-        navbar.addEventListener('animationend', () => {
-            console.log('running animation en')
-            navbar.removeAttribute('visible')
-            overlay.removeAttribute('visible')
-            navbar.removeAttribute('closing')
-            overlay.removeAttribute('closing')
-        }, { once: true })
+
+    if(!emailRegex.test(data.email)) {
+        hasErrors = true
+        emailError.textContent = 'Invalid email'
+        emailError.setAttribute('visible', "")
+        
     }
+
+    if(data.message.length === 0) {
+        console.log('Message is empty')
+        hasErrors = true
+        messageError.textContent = "There is no message to send"
+        messageError.setAttribute('visible', '')
+        
+    }
+
     
+
+    if(hasErrors) return
+    // Removing animations if there is no errors
+    if(emailError.hasAttribute('visible')) {
+        emailError.setAttribute('closing', '')
+        emailError.addEventListener('animationend', () => {
+            emailError.removeAttribute('visible')
+            emailError.removeAttribute('closing')
+        }, { once : true})
+    }
+
+    if(messageError.hasAttribute('visible')) {
+        messageError.setAttribute('closing', '')
+        messageError.addEventListener('animationend', () => {
+            messageError.removeAttribute('visible')
+            messageError.removeAttribute('closing')
+        }, { once : true })
+    }
+    // Theres no errors continue the logic here
+}
 
 navItems.forEach(item => item.addEventListener("click", menuClickHandler))
 hamburgerButton.addEventListener("click", openNavbar)
@@ -73,4 +127,6 @@ skillCategoryDropdownButtons.forEach((element) => {
         dropdownContent.toggleAttribute('visible')
     })
 })
+
+contactForm.addEventListener('submit', onSubmitContactForm)
 
